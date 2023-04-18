3a982e020cde86910993664ebd0edf86fb760c4b
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-04-18 00:42:09 -0500

    updated dom.js

--- dom.js
+++ dom.js
@@ -2,40 +2,6 @@
 
 import movies from "./moviesList.js";
 
-const watchMovieBtn = document.querySelectorAll(".watch-movie");
-const moviesList = document.querySelectorAll(".movie-modal");
-const overlay = document.querySelector(".overlay");
-const closeMovie = document.querySelectorAll(".close-modal");
-const trailerVideo = document.querySelectorAll(".trailer"); //not used yet
-
-const watchMovieHandler = (index) => {
-  moviesList[index].classList.remove("hidden");
-  moviesList[index].classList.add("watch-flex");
-  overlay.classList.remove("hidden");
-};
-
-const closeMovieHandler = (index) => {
-  moviesList[index].classList.add("hidden");
-  moviesList[index].classList.remove("watch-flex");
-  overlay.classList.add("hidden");
-};
-
-closeMovie.forEach((el, index) => {
-  el.addEventListener("click", () => closeMovieHandler(index));
-});
-
-watchMovieBtn.forEach((el, index) => {
-  el.addEventListener("click", () => watchMovieHandler(index));
-});
-
-document.querySelector("body").addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    moviesList.forEach((el, index) => {
-      el.addEventListener("click", closeMovieHandler(index));
-    });
-  }
-});
-
 //--- Function to create movie tiles and modals---//
 function createMoviesModals(moviesList) {
   const modalContainer = document.querySelector(".modal-container");
@@ -126,3 +92,37 @@ function createMoviesModals(moviesList) {
 }
 
 createMoviesModals(movies);
+
+const watchMovieBtn = document.querySelectorAll(".watch-movie");
+const moviesList = document.querySelectorAll(".movie-modal");
+const overlay = document.querySelector(".overlay");
+const closeMovie = document.querySelectorAll(".close-modal");
+const trailerVideo = document.querySelectorAll(".trailer"); //not used yet
+
+const watchMovieHandler = (index) => {
+  moviesList[index].classList.remove("hidden");
+  moviesList[index].classList.add("watch-flex");
+  overlay.classList.remove("hidden");
+};
+
+const closeMovieHandler = (index) => {
+  moviesList[index].classList.add("hidden");
+  moviesList[index].classList.remove("watch-flex");
+  overlay.classList.add("hidden");
+};
+
+closeMovie.forEach((el, index) => {
+  el.addEventListener("click", () => closeMovieHandler(index));
+});
+
+watchMovieBtn.forEach((el, index) => {
+  el.addEventListener("click", () => watchMovieHandler(index));
+});
+
+document.querySelector("body").addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    moviesList.forEach((el, index) => {
+      el.addEventListener("click", closeMovieHandler(index));
+    });
+  }
+});
